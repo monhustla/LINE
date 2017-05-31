dd44c38bb9deb0a51b73661be4806d32a372a0a9
monhustla <rdavis23344@gmail.com>
2017-05-31 18:38:18 -0500

    Update sampletwo.py

--- sampletwo.py
+++ sampletwo.py
@@ -1,7 +1,7 @@
 from line import LineClient, LineGroup, LineContact
 
 try:
-    client = LineClient("shitmiikesays", "Bboy123!")
+    client = LineClient("mythlegend101@gmail.com", "Bboy123!")
     #client = LineClient(authToken="AUTHTOKEN")
 except:
     print "Login Failed"
