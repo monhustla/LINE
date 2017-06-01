fd95ca12d55e2fdfea242917c8689affbefa8ddb
monhustla <rdavis23344@gmail.com>
2017-05-31 20:03:42 -0500

    Update sampletwo.py

--- sampletwo.py
+++ sampletwo.py
@@ -1,8 +1,21 @@
 from line import LineClient, LineGroup, LineContact
 
+try:
 client= LineClient("mythlegend101@gmail.com", "Bboy123!")
-authToken=client.authToken
-print authToken
 
-client = LineClient(authToken=authToken)
+except:
+    print "Login Failed"
 
+while True:
+    op_list = []
+
+    for op in client.longPoll():
+        op_list.append(op)
+
+    for op in op_list:
+        sender   = op[0]
+        receiver = op[1]
+        message  = op[2]
+        
+        msg = message.text
+        receiver.sendMessage("[%s] %s" % (sender.name, msg))
